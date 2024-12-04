13e84cff5d744dd205c9673971a0bc47b5a23a78
Man Nguyen <minhman2202@gmail.com>
2024-12-04 16:59:40 +0700

    initialize first react proj

---
+++ react-essentials/src/App.jsx
@@ -0,0 +1,42 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
+
+function App() {
+  const [dynamicContent, setDynamicContent] = useState("Dynamic Content!");
+
+  function handleSelect(tabName) {
+    setDynamicContent(`You clicked on ${tabName}`);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
+          </menu>
+          {dynamicContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
