b72eb2e40e14517f64bde20194cdc43711912e10
Man Nguyen <minhman2202@gmail.com>
2024-12-12 13:51:21 +0700

    use State to switch between NoProjectSelected & NewProject components

--- project-management-tool/src/App.jsx
+++ project-management-tool/src/App.jsx
@@ -1,12 +1,35 @@
+import {useState} from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject/>;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
