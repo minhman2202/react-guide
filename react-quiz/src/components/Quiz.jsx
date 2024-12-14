925f6e9b9474a19b271162beb72ae017f2a64038
Man Nguyen <minhman2202@gmail.com>
2024-12-14 14:56:02 +0700

    shuffing answers and adding quiz logic

--- react-quiz/src/components/Quiz.jsx
+++ react-quiz/src/components/Quiz.jsx
@@ -1,23 +1,36 @@
 import {useState} from "react";
 
+import quizCompleteImg from '../assets/quiz-complete.png';
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
   }
 
+  if (quizIsCompleted) {
+    return (<div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon"/>
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(() => Math.random() - 0.5);
+
   return (<div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (<li key={index} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>))}
+          {shuffledAnswers.map((answer, index) => (<li key={index} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>
+              {answer}
+            </button>
+          </li>))}
         </ul>
       </div>
     </div>
