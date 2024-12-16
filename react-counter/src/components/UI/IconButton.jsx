d925afda7a9e072ae2b53b1a4c2153ea90079029
Man Nguyen <minhman2202@gmail.com>
2024-12-16 15:40:46 +0700

    using callback (together with memo) to avoid IconButton from being rendered for each click

--- react-counter/src/components/UI/IconButton.jsx
+++ react-counter/src/components/UI/IconButton.jsx
@@ -1,13 +1,17 @@
-import { log } from '../../log.js';
+import {memo} from "react";
 
-export default function IconButton({ children, icon, ...props }) {
+import {log} from '../../log.js';
+
+const IconButton = memo(function IconButton({children, icon, ...props}) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
   return (
     <button {...props} className="button">
-      <Icon className="button-icon" />
+      <Icon className="button-icon"/>
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
