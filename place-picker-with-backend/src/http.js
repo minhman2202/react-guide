44cdd6e703d141bd51f04e125f90ef83066ff0d8
Man Nguyen <minhman2202@gmail.com>
2025-02-25 13:25:23 +0700

    place-picker-with-backend: storing user selected places in the backend

--- place-picker-with-backend/src/http.js
+++ place-picker-with-backend/src/http.js
@@ -5,6 +5,29 @@ export async function fetchAvailablePlaces() {
   if (!response.ok) {
     throw new Error('Failed to fetch places!');
   }
-
   return data.places;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch('http://localhost:3000/user-places');
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error('Failed to fetch user places!');
+  }
+  return data.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({places}),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error('Failed to update user places!');
+  }
+  return data;
 }
