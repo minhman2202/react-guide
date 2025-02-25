347560c833fc45361162402c858032959c24e7ff
Man Nguyen <minhman2202@gmail.com>
2025-02-25 10:32:38 +0700

    place-picker-with-backend: fetching data from the backend server

--- place-picker-with-backend/src/components/AvailablePlaces.jsx
+++ place-picker-with-backend/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import {useState, useEffect} from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // useEffect to ensure there is no infinite loop when the component is rendered because of state change
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      return response.json();
+    }).then((data) => {
+      setAvailablePlaces(data.places);
+    });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
