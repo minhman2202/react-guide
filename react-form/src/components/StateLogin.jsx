c2078486d32c9d08f806b6704004795d6d460ef3
Man Nguyen <minhman2202@gmail.com>
2025-02-26 11:42:23 +0700

    react-form: getting user input via refs

--- react-form/src/components/StateLogin.jsx
+++ react-form/src/components/StateLogin.jsx
@@ -1,16 +1,23 @@
-import { useRef } from 'react';
+import {useState} from 'react';
 
 export default function Login() {
 
-  const email = useRef();
-  const password = useRef();
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(email.current.value, password.current.value);
+    console.log(enteredValues);
+  }
 
-    email.current.value = '';
-    password.current.value = '';
+  function handleInputChange(event) {
+    const {name, value} = event.target;
+    setEnteredValues(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   }
 
   return (
@@ -20,12 +27,13 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" ref={email} />
+          <input id="email" type="email" name="email" onChange={handleInputChange} value={enteredValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={password} />
+          <input id="password" type="password" name="password" onChange={handleInputChange}
+                 value={enteredValues.password}/>
         </div>
       </div>
 
