bc5990148d07f8129591876057de663c11b4cad1
Man Nguyen <minhman2202@gmail.com>
2025-02-26 15:25:14 +0700

    react-form: building reusable input component

--- react-form/src/components/StateLogin.jsx
+++ react-form/src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import Input from "./Input.jsx";
 
 export default function Login() {
 
@@ -13,6 +14,7 @@ export default function Login() {
   });
 
   const isEmailInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const isPasswordInvalid = didEdit.password && enteredValues.password.trim().length < 6;
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -50,20 +52,18 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email"
-                 onBlur={handleInputBlur}
-                 onChange={handleInputChange}
-                 value={enteredValues.email}/>
-          <div className="control-error">{isEmailInvalid && <p>Please enter a valid email address.</p>}</div>
-        </div>
+        <Input label="Email" id="email" type="email" name="email"
+               error={isEmailInvalid && 'Please enter a valid email address.'}
+               onBlur={handleInputBlur}
+               onChange={handleInputChange}
+               value={enteredValues.email}/>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={handleInputChange}
-                 value={enteredValues.password}/>
-        </div>
+        <Input label="Password" id="password" type="password" name="password"
+               error={isPasswordInvalid && 'Password must be at least 6 characters long.'}
+               onBlur={handleInputBlur}
+               onChange={handleInputChange}
+               value={enteredValues.password}/>
+
       </div>
 
       <p className="form-actions">
