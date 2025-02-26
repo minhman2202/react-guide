e1157425bd9478fa9b842ba8021834a752fb430c
Man Nguyen <minhman2202@gmail.com>
2025-02-26 12:33:48 +0700

    react-form: validating input on every keystroke via state

--- react-form/src/components/StateLogin.jsx
+++ react-form/src/components/StateLogin.jsx
@@ -7,6 +7,8 @@ export default function Login() {
     password: ''
   });
 
+  const isEmailInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
@@ -34,6 +36,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" onChange={handleInputChange} value={enteredValues.email}/>
+          <div className="control-error">{isEmailInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
