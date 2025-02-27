885246006a16fc4d8f2b2a8345274ceb5212d7bd
Man Nguyen <minhman2202@gmail.com>
2025-02-27 14:05:33 +0700

    react-form-action (advanced): using 'pending' state from useActionState() to disable/enable upvote/downvote buttons

--- react-form-action-with-backend/src/components/Opinion.jsx
+++ react-form-action-with-backend/src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import {use} from 'react';
+import {use, useActionState} from 'react';
 import {OpinionsContext} from "../store/opinions-context.jsx";
 
 export function Opinion({opinion: {id, title, body, userName, votes}}) {
@@ -15,6 +15,9 @@ export function Opinion({opinion: {id, title, body, userName, votes}}) {
     await downvoteOpinion(id);
   }
 
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction, null);
+  const [downvoteFormState, downvoteFormAction, downvotePending] = useActionState(downvoteAction, null);
+
   return (
     <article>
       <header>
@@ -23,7 +26,7 @@ export function Opinion({opinion: {id, title, body, userName, votes}}) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upvoteAction}>
+        <button formAction={upvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -43,7 +46,7 @@ export function Opinion({opinion: {id, title, body, userName, votes}}) {
 
         <span>{votes}</span>
 
-        <button formAction={downvoteAction}>
+        <button formAction={downvoteFormAction} disabled={upvotePending || downvotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
