3096857f95b502b1dabb676e289248503d09ff92
Man Nguyen <minhman2202@gmail.com>
2025-02-27 09:50:06 +0700

    react-form-action: managing Form-dependent State with useActionState()

--- react-form-action/src/components/Signup.jsx
+++ react-form-action/src/components/Signup.jsx
@@ -1,18 +1,73 @@
+import {useActionState} from "react";
+
+import {isEmail, isNotEmpty, isEqualToOtherValue, hasMinLength} from "../util/validation.js";
+
 export default function Signup() {
+  function signupAction(prevFormState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
+    const firstName = formData.get('first-name');
+    const lastName = formData.get('last-name');
+    const role = formData.get('role');
+    const terms = formData.get('terms');
+    const acquisitionChannel = formData.getAll('acquisition');
+
+    // validate the data
+    let errors = [];
+    if (!isEmail(email)) {
+      errors.push('Invalid email address.');
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push('Password must be at least 6 characters long.');
+    }
+
+    if (!isNotEmpty(confirmPassword) || !isEqualToOtherValue(password, confirmPassword)) {
+      errors.push('Passwords do not match.');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push('Please provide both your first and last name.');
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push('Please select a role.');
+    }
+
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.');
+    }
+
+    if (acquisitionChannel.length === 0) {
+      errors.push('Please select at least one acquisition channel.');
+    }
+
+    if (errors.length > 0) {
+      return {errors};
+    }
+
+    // if we get here, the data is valid
+    console.log(Object.fromEntries(formData.entries()));
+    return {errors: null};
+  }
+
+  const [formState, formAction] = useActionState(signupAction, {error: null});
+
   return (
-    <form>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email"/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"/>
         </div>
 
         <div className="control">
@@ -25,17 +80,17 @@ export default function Signup() {
         </div>
       </div>
 
-      <hr />
+      <hr/>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name"/>
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name"/>
         </div>
       </div>
 
@@ -73,17 +128,23 @@ export default function Signup() {
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other"/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms"/>I
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && <ul className="error">
+        {formState.errors.map((error, index) => (
+          <li key={index}>{error}</li>
+        ))}
+      </ul>}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
