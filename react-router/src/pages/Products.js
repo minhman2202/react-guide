b753313fe5f22c7d2064a8283e920c0c43f022f9
Man Nguyen <minhman2202@gmail.com>
2025-03-04 09:12:26 +0700

    react-router: rendering list of products dynamically

--- react-router/src/pages/Products.js
+++ react-router/src/pages/Products.js
@@ -1,14 +1,21 @@
+import {Link} from 'react-router-dom'
+
+const PRODUCTS = [
+  {id: 1, title: 'A Book'},
+  {id: 2, title: 'A Carpet'},
+  {id: 3, title: 'An Online Course'}
+];
+
 export default function ProductsPage() {
   return (
     <>
       <h1>The Products Page</h1>
       <ul>
-        <li>
-          <a href="/products/p1">A Book</a>
-        </li>
-        <li>
-          <a href="/products/p2">A Carpet</a>
-        </li>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
       </ul>
     </>);
 }
