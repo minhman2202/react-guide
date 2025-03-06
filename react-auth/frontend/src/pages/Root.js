59b29c4aaf253015949a302716c56173b726d38b
Man Nguyen <minhman2202@gmail.com>
2025-03-06 15:23:57 +0700

    react-auth: Managing the Token Expiration

--- react-auth/frontend/src/pages/Root.js
+++ react-auth/frontend/src/pages/Root.js
@@ -1,16 +1,38 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {Outlet, useLoaderData, useNavigation, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {useEffect} from "react";
+import {getTokenDuration} from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
 
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, {action: '/logout', method: 'post'});
+      return;
+    }
+
+    // logout after 1 hour
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'post'});
+    }, tokenDuration);
+  }, [token, submit]);
+
   return (
     <>
-      <MainNavigation />
+      <MainNavigation/>
       <main>
         {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
+        <Outlet/>
       </main>
     </>
   );
