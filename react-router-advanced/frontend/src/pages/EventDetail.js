7aa6f6c91d8ee525ce2abc215ba4a54084b8b726
Man Nguyen <minhman2202@gmail.com>
2025-03-06 10:14:14 +0700

    react-router-advanced: deleting an event (submitting form programmatically)

--- react-router-advanced/frontend/src/pages/EventDetail.js
+++ react-router-advanced/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import {useRouteLoaderData} from "react-router-dom";
+import {redirect, useRouteLoaderData} from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -18,3 +18,18 @@ export async function loader({request, params}) {
     return response;
   }
 }
+
+export async function action({request, params}) {
+  const eventId = params.eventId;
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+    headers: {
+      'Content-Type': 'application/json'
+    }});
+
+    if (!response.ok) {
+      throw Response.json({message: 'Could not delete event.'}, {status: 500});
+    } else {
+      return redirect('/events');
+    }
+  }
