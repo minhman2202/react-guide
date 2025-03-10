a1df05479c0a6379a5043bdb9aad6398c6f30810
Man Nguyen <minhman2202@gmail.com>
2025-03-10 13:45:20 +0700

    react-events: implementing create new event functionality

--- react-events/src/App.jsx
+++ react-events/src/App.jsx
@@ -3,42 +3,41 @@ import {
   RouterProvider,
   createBrowserRouter,
 } from 'react-router-dom';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {QueryClientProvider} from "@tanstack/react-query";
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import {queryClient} from "./util/http.js";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/events" />,
+    element: <Navigate to="/events"/>,
   },
   {
     path: '/events',
-    element: <Events />,
+    element: <Events/>,
 
     children: [
       {
         path: '/events/new',
-        element: <NewEvent />,
+        element: <NewEvent/>,
       },
     ],
   },
   {
     path: '/events/:id',
-    element: <EventDetails />,
+    element: <EventDetails/>,
     children: [
       {
         path: '/events/:id/edit',
-        element: <EditEvent />,
+        element: <EditEvent/>,
       },
     ],
   },
 ]);
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
