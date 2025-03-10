417c41421ea2a8aafda6cfef36d8f183fb3c8a61
Man Nguyen <minhman2202@gmail.com>
2025-03-10 14:27:00 +0700

    react-events: implementing load event detail functionality

--- react-events/src/components/Events/EventDetails.jsx
+++ react-events/src/components/Events/EventDetails.jsx
@@ -1,34 +1,71 @@
-import { Link, Outlet } from 'react-router-dom';
+import {Link, Outlet, useParams} from 'react-router-dom';
+import {useQuery} from "@tanstack/react-query";
 
 import Header from '../Header.jsx';
+import {fetchEvent} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
+  const params = useParams();
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+  });
+
+  let content;
+  if (isPending) {
+    content = <div id="event-details-content" className="center">
+      <p>Fetching event data...</p>
+    </div>;
+  }
+
+  if (isError) {
+    content = <div id="event-details-content" className="center">
+      <ErrorBlock title="An error occurred" message={error.info?.message || 'Failed to load event.'}/>
+    </div>;
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+
+    content = (
+      <>
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title}/>
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate} @ {data.time}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{data.description}</p>
           </div>
         </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Outlet/>
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">
+        {content}
       </article>
     </>
   );
