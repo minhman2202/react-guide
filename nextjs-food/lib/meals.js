5316d5c7047a1316bfbbac83fb1a8cac6a3e4701
Man Nguyen <minhman2202@gmail.com>
2025-03-10 23:41:21 +0700

    nextjs-food: Fetching Meal data By Leveraging NextJS & Fullstack Capabilities

---
+++ nextjs-food/lib/meals.js
@@ -0,0 +1,10 @@
+import sql from 'better-sqlite3';
+
+const db = sql('meals.db');
+
+export async function getMeals() {
+  // adding extra delay to simulate a slow network
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return db.prepare('SELECT * FROM meals').all();
+}
