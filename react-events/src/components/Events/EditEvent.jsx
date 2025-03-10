0bfa7126d9104ae1714232f5e51c8f9993aea127
Man Nguyen <minhman2202@gmail.com>
2025-03-10 15:08:25 +0700

    react-events: loading event detail for editing

--- react-events/src/components/Events/EditEvent.jsx
+++ react-events/src/components/Events/EditEvent.jsx
@@ -1,20 +1,48 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
+import {useQuery} from "@tanstack/react-query";
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import {fetchEvent} from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const params = useParams();
+
+  const {data, isPending, isError, error} = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({id: params.id, signal}),
+  })
+
+  function handleSubmit(formData) {
+  }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+  if (isPending) {
+    content = <div className="center"><LoadingIndicator/></div>;
+  }
+
+  if (isError) {
+    content = (<>
+      <ErrorBlock title="An error occurred" message={error.info?.message || 'Failed to load event.'}/>
+      <div className="form-actions">
+        <Link to="../" className="button-text">
+          Okay
+        </Link>
+      </div>
+    </>);
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +50,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
