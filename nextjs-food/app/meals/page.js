50838127931551daceb10a9ee025e2caccef3590
Man Nguyen <minhman2202@gmail.com>
2025-03-11 08:22:02 +0700

    nextjs-food: Using Suspense & Streamed Responses For Granular Loading State Management

--- nextjs-food/app/meals/page.js
+++ nextjs-food/app/meals/page.js
@@ -1,12 +1,16 @@
+import {Suspense} from "react";
 import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
-import classes from './page.module.css';
 import {getMeals} from "@/lib/meals";
+import classes from './page.module.css';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals}/>
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +23,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+          <Meals/>
+        </Suspense>
       </main>
     </>
   );
