8a88c64329ae909eafa3601331cbb5440b017fd5
Man Nguyen <minhman2202@gmail.com>
2025-03-17 11:32:02 +0700

    react-testing: working with mocks

--- react-testing/src/components/Async.test.js
+++ react-testing/src/components/Async.test.js
@@ -3,6 +3,12 @@ import {render, screen} from "@testing-library/react";
 
 describe('Async component', () => {
   it('renders posts if request succeeds', async () => {
+    // create a mock function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: 'p1', title: 'First post'}],
+    });
+
     // Arrange
     render(<Async/>);
 
